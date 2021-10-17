cd45bbc1b29fd20866bcf25507b24850ba33e0ce
Luntu M <luntu.mavukz35@gmail.com>
2021-10-17 02:21:43 +0200

    ADDED: detail screen

---
+++ StackOverflowApp/Screens/Detailed Screen/View Controller/StackOverflowDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StackOverflowDetailViewController.swift
+//  StackOverflowApp
+//
+//  Created by Luntu Mavukuza on 2021/10/17.
+//
+
+import Foundation
+import UIKit
+
+class StackOverflowDetailViewController: UIViewController {
+    
+}
